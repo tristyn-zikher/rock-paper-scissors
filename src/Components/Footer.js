2ecae59ce029f022a031fd6382c70305039ca271
tristyntech <tristyntech@gmail.com>
2019-04-07 21:49:00 -0500

    editing text

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer id="sticky-footer" className="py-4 bg-dark text-white-50">
       <div className="container text-center">
-        <h6>React/Bootstrap - Mobile Friendly</h6>
+        <h6>React/Bootstrap</h6>
         <h6>Created by Tristyn Leos - (512) 956-1160</h6>
       </div>
     </footer>
